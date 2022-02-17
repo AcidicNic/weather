23b9c3393c8b52abc02d8b845929005d0d556d00
Nicole Abadie <nic4096@gmail.com>
2022-02-16 18:15:31 -0800

    added getWeatherPromise and error handling

--- index.js
+++ index.js
@@ -2,14 +2,30 @@
  * getWeather()
  */
 
-// getWeather() via callback
-export function getWeather(apiKey, zip, callback) {
+// getWeather via callback
+export function getWeatherCallback(apiKey, zip, weatherCallback, errCallback) {
 	const units = 'imperial';
-	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+	const path = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&zip=${zip}&units=${units}`;
 	fetch(path)
 		.then(res => res.json())
 		.then(json => {
-			callback(json);
+			if (json.cod !== 200) {
+				return errCallback({ name: 'Invalid Zip Code', message: 'Try Again' });
+			}
+
+			weatherCallback(json);
 		})
-	.catch(err => console.log(err.message));
+	.catch(err => errCallback(err));
+}
+
+// getWeather via promise
+export const getWeatherPromise = async (apiKey, zip) => {
+	const units = 'imperial';
+	const path = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&zip=${zip}&units=${units}`;
+	const res = await fetch(path);
+	const weatherJson = await res.json();
+	if (weatherJson.cod !== 200) {
+		return Promise.reject({ name: 'Invalid Zip Code', message: 'Try Again' });
+	}
+	return weatherJson
 }
